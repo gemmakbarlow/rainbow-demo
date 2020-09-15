c8f919d5abea2c8e6789abf3372e502462865b98
Gemma Barlow <gemma@goradar.com>
2020-09-14 21:13:27 -0400

    Initial commit of Rainbow Demo project

---
+++ rainbow-demo/rainbow-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rainbow-demo
+//
+//  Created by Gemma Barlow on 9/14/20.
+//  Copyright © 2020 gemmakbarlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
